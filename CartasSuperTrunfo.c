b2bcab8d56b17ab5ddfa218d99f1abc4ad63a9e3
RonaldoBergueJr <ronaldobergue74@gmail.com>
2025-10-02 22:49:25 +0000

    Eu fiz o Desafio 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,152 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado[50];
+    char Codigo[5];
+    char Cidade[50];
+    int  Populacao;
+    float Area;
+    float Pib;
+    int Turisticos;
+    // Área para entrada de dados
+  printf("Carta 01\n");
 
-  // Área para entrada de dados
+printf("Estado: ");
+scanf("%s", Estado);
+    
+printf("Código: ");
+scanf("%s", Codigo);
 
+printf("Cidade: ");
+scanf("%s", Cidade);
+
+printf("População: ");
+scanf("%d", &Populacao);
+
+printf("Área: ");
+scanf("%f", &Area);
+
+printf("PIB: ");
+scanf("%f", &Pib);
+
+printf("Pontos Turísticos: ");
+scanf("%d", &Turisticos);
+
+
+    
+
+  printf("Carta 02\n");
+
+printf("Estado: ");
+scanf("%s", Estado);
+    
+printf("Código: ");
+scanf("%s", Codigo);
+
+printf("Cidade: ");
+scanf("%s", Cidade);
+
+printf("População: ");
+scanf("%d", &Populacao);
+
+printf("Área: ");
+scanf("%f", &Area);
+
+printf("PIB: ");
+scanf("%f", &Pib);
+
+printf("Pontos Turísticos: ");
+scanf("%d", &Turisticos);
+
+
+  printf("Carta 03\n");
+
+printf("Estado: ");
+scanf("%s", Estado);
+    
+printf("Código: ");
+scanf("%s", Codigo);
+
+printf("Cidade: ");
+scanf("%s", Cidade);
+
+printf("População: ");
+scanf("%d", &Populacao);
+
+printf("Área: ");
+scanf("%f", &Area);
+
+printf("PIB: ");
+scanf("%f", &Pib);
+
+printf("Pontos Turísticos: ");
+scanf("%d", &Turisticos);
+
+
+    
+
+  printf("Carta 04\n");
+
+printf("Estado: ");
+scanf("%s", Estado);
+    
+printf("Código: ");
+scanf("%s", Codigo);
+
+printf("Cidade: ");
+scanf("%s", Cidade);
+
+printf("População: ");
+scanf("%d", &Populacao);
+
+printf("Área: ");
+scanf("%f", &Area);
+
+printf("PIB: ");
+scanf("%f", &Pib);
+
+printf("Pontos Turísticos: ");
+scanf("%d", &Turisticos);
   // Área para exibição dos dados da cidade
+printf("\n===== Carta 1 Cadastrada =====\n");
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Área: %d\n", Populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões\n", Pib);
+    printf("Pontos Turísticos: %d\n", Turisticos);
 
+
+
+    printf("\n===== Carta 2 Cadastrada =====\n");
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Área: %d\n", Populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões\n", Pib);
+    printf("Pontos Turísticos: %d\n", Turisticos);
+
+    
+    printf("\n===== Carta 3 Cadastrada =====\n");
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Área: %d\n", Populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões\n", Pib);
+    printf("Pontos Turísticos: %d\n", Turisticos);
+
+
+
+    printf("\n===== Carta 4 Cadastrada =====\n");
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", Codigo);
+    printf("Cidade: %s\n", Cidade);
+    printf("Área: %d\n", Populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões\n", Pib);
+    printf("Pontos Turísticos: %d\n", Turisticos);
 return 0;
 } 
